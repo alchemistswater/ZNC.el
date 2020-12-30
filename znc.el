40ee1d66d7c9f989310400500d56b9b582b7377f
alchemists water <alchemistswater@pop-os.localdomain>
2020-12-30 13:24:03 -0500

    added the password expose fix and fixed the byte compilation warning

--- znc.el
+++ znc.el
@@ -115,7 +115,8 @@ some of the quirks that arise from using it with a naive ERC. "
           (with-current-buffer returning
             (znc-set-name wants-name)
             (rename-buffer wants-name))
-          (get-buffer wants-name))
+          (when-let ((buffer (get-buffer wants-name)))
+	    buffer))
       returning)))
 
 (defadvice erc-kill-channel (around znc-maybe-dont-part first nil activate)
@@ -212,20 +213,20 @@ to the matching values for the endpoint"
       (make-local-variable 'znc-buffer-name)
       (setf znc-buffer-name znc-name))))
 
-(defun znc-erc-connect (endpoint)
-  (message "Called with: %s" endpoint)
-  (with-endpoint endpoint
-                 (message "Have endpoint: %s" endpoint)
-                 (let* ((buffer (znc-network-buffer-name slug))
-                        (erc-fun (if ssl znc-erc-ssl-connector znc-erc-connector))
-                        (erc-args `(:server ,host :port ,port
-                                    :nick ,user :password ,(format "%s:%s" user pass)))
-                        (erc-buffer (apply erc-fun erc-args)))
-                   (when (get-buffer buffer)
-                     (znc-kill-buffer-always buffer))
-                   (znc-set-name buffer erc-buffer)
-                   (with-current-buffer erc-buffer
-                     (rename-buffer buffer)))))
+ (let ((message-endpoint (append (butlast endpoint) '("***password***"))))
+    (message "Called with: %s" message-endpoint)
+    (with-endpoint endpoint
+                   (message "Have endpoint: %s" message-endpoint)
+                   (let* ((buffer (znc-network-buffer-name slug))
+                          (erc-fun (if ssl znc-erc-ssl-connector znc-erc-connector))
+                          (erc-args `(:server ,host :port ,port
+                                              :nick ,user :password ,(format "%s:%s" user pass)))
+                          (erc-buffer (apply erc-fun erc-args)))
+                     (when (get-buffer buffer)
+                       (znc-kill-buffer-always buffer))
+                     (znc-set-name buffer erc-buffer)
+                     (with-current-buffer erc-buffer
+                       (rename-buffer buffer))))))
 
 (defun znc-prompt-string-or-nil (prompt &optional completions default require-match)
   (let* ((string (completing-read (concat prompt ": ") completions nil require-match default))
